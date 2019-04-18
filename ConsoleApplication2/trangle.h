64242a932dac228855c73060bd14fb58da28c4f1
Эчис-Баадыр Бегзи <echis.begzi@outlook.com>
2019-04-18 12:49:12 +0700

    newClass

---
+++ ConsoleApplication2/trangle.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include "iostream"
+#include "fstream"
+
+using namespace std;
+
+
+struct trangle
+{
+	int x1,x2, y1,y2,z1,z2; // центр круга
+};
+
+void InTrangle(trangle &tr, ifstream &ifst);
+void OutTrangle(trangle &tr, ofstream &ofst);
